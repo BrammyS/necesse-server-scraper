365fec16e7c73eb84187609fc64c3d40b05aa3b9
Bram <bram.esendam@gmail.com>
2024-08-17 11:21:50 +0200

    added new xpath

--- src/NecesseScraper/Services/Implementation/ServerScraper.cs
+++ src/NecesseScraper/Services/Implementation/ServerScraper.cs
@@ -24,7 +24,7 @@ public class ServerScraper : IServerScraper
         webDriver.Navigate().GoToUrl("https://necessegame.com/server");
         _logger.LogInformation("Scraper finished after {Time:F1}ms", (Stopwatch.GetTimestamp() - startTime) / (double)Stopwatch.Frequency * 1000);
 
-        var versionElement = webDriver.FindElement(By.XPath("/html/body/div[2]/div/div/div[4]/div/p[1]/a[4]"));
+        var versionElement = webDriver.FindElement(By.XPath("/html/body/div[2]/div/div/div[4]/div/p[1]/a[3]"));
         _logger.LogInformation("Element found, parsing version");
 
         try
